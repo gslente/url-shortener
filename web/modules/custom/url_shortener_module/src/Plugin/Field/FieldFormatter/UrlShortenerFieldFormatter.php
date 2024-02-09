67c22a4e3f9e0dbb854b64c56f35dc35031ae0b7
gslente <gabrielseth.ente@gmail.com>
2024-02-09 19:18:00 +0800

    Update composer packages -add service for URLShortener

--- web/modules/custom/url_shortener_module/src/Plugin/Field/FieldFormatter/UrlShortenerFieldFormatter.php
+++ web/modules/custom/url_shortener_module/src/Plugin/Field/FieldFormatter/UrlShortenerFieldFormatter.php
@@ -2,35 +2,36 @@
 
 namespace Drupal\url_shortener_module\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Field\{FieldItemBase, FieldStorageDefinitionInterface};
+use Drupal\Core\Field\{FieldItemBase, FieldStorageDefinitionInterface, FieldItemListInterface};
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\TypedData\DataDefinition;
-
+use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
 
 /**
 * Plugin implementation of the 
 *    'URL Shortener Field Formatter' formatter.
 *
+* Shortens the URL Link field.
+*
 * @FieldFormatter(
 *   id = "url_shortener_field_formatter",
 *   label = @Translation("URL Shortener Field Formatter"),
 *   field_types = {
-*     "string_long",
-*     "string",
 *     "link",
 *   }
 * )
 */
-class UrlShortenerFieldFormatter extends FormatterBase  {
+class UrlShortenerFieldFormatter extends LinkFormatter  {
     public function viewElements(FieldItemListInterface $items, $langcode) {
-        $elements = [];
+        $element = parent::viewElements($items, $langcode);
+        $settings = $this->getSettings();
     
-        foreach ($items as $delta => $item) {
-            $elements[$delta] = [
-                '#markup' => "Hello formatter, the value is $item->value",
-            ];
-        }
+        $id = $items->getEntity()->id();
+        foreach ($element as $index => &$item) {
+            $url_shortener_service = \Drupal::service('url_shortener_module.url_shortener');
+            $url_shortener_service->shorten_url($id, $item);
+        }     
     
-        return $elements;
+        return $element;
     }
 }
